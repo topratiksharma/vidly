ec7d2a900d28d54f14734a3e383bafb86564c170
Pratik Sharma <pratiksharma@live.in>
2021-09-05 16:00:15 +0530

    Refactoring

--- src/components/movies/moviesTable.jsx
+++ src/components/movies/moviesTable.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Like from '../common/like';
-import Table from '../common/table/Table';
+import Table from '../common/table/table';
 
 class MoviesTable extends Component {
   columns = [
